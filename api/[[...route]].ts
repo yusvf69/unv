fb0427b64cf782cecfc7708bae1d9fcb38d25c0a
youssef mohamed <youssefmohamed@youssefs-MacBook-Pro.local>
2026-04-30 23:05:18 +0300

    Use Express app directly in Vercel serverless function

--- api/[[...route]].ts
+++ api/[[...route]].ts
@@ -1,18 +1,5 @@
-import { handle } from "hono/vercel";
-import { Hono } from "hono";
-import { cors } from "hono/cors";
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
-import * as schema from "@workspace/db/schema";
+import expressApp from "../artifacts/api-server/src/app";
 
-const sql = neon(process.env.DATABASE_URL!);
-const db = drizzle(sql, { schema });
-
-const app = new Hono();
-
-app.use("/*", cors());
-
-export const GET = handle(app);
-export const POST = handle(app);
-export const PUT = handle(app);
-export const DELETE = handle(app);
+export default function handler(req: any, res: any) {
+  expressApp(req, res);
+}
